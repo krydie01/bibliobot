8c1584665f7a43776366a877e480ddd829a9c2bd
krydie01 <limonserver@mail.ru>
2018-10-22 10:22:46 +0900

    edit index

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api')
-
 const TOKEN = '749224567:AAHnljGebf2cFmx9_YxBsTRThHQPpTN16uk'
 
+console.log('Bot has been started')
+
 const bot = new TelegramBot(TOKEN, {
-  polling: true
+  polling: {
+    interval: 300,
+    autoStart: true,
+    params: {
+      timeout: 10
+    }
+  }
 })
 
 bot.on('message', (msg) => {
   console.log(msg)
-    bot.sendMessage(msg.chat.id, 'Здравствуйте' + msg.from.first_name + '! ' + 'Я бот Библиотек Благовещенска! ')
+  bot.sendMessage(msg.chat.id, 'Здравствуйте ' + msg.from.first_name + '! ' + 'Я бот Библиотек Благовещенска! ')
 })
